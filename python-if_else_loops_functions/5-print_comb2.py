f5e674746e39e8ded30f27ba37bb2df120b36027
omega-jean <jeanalex1405@gmail.com>
2024-05-07 13:27:25 +0000

    modify code

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 99:
-        print('{}, '.format(f"{i:02}"), end="")
+        print('{}'.format(f"{i:02}"), end=", ")
     else:
         print('{}'.format(f"{i:02}"), end="")
 print()

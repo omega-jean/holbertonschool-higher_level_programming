6081eb811b58c3aa445045997aa0286d9131dfce
omega-jean <jeanalex1405@gmail.com>
2024-05-13 11:18:37 +0000

    Reverse code

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,9 +2,9 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     if idx < 0:
-        return my_list
+        return new_list
     elif idx >= len(my_list):
-        return my_list
+        return new_list
     else:
-        my_list[idx] = element
+        new_list[idx] = element
         return new_list

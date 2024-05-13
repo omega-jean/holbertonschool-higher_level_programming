ddac6e2199a76ba5c16146055c61d5e445ca600b
omega-jean <jeanalex1405@gmail.com>
2024-05-13 13:51:33 +0000

    fix pycodestyle

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for j in matrix:
-        for i in range (len(j)):
+        for i in range(len(j)):
             print("{:d}".format(j[i]), end="")
             if i != len(j) - 1:
                 print(" ", end="")

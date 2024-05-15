cff4fb944e4f8cbd9bcb3b9173685c553f4e25c3
omega-jean <jeanalex1405@gmail.com>
2024-05-15 14:06:27 +0000

    fix pycodestyle

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,10 +2,10 @@
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
-    
+
     value_max = None
     best_key = None
-    
+
     for key, value in a_dictionary.items():
         if value_max is None or value > value_max:
             value_max = value

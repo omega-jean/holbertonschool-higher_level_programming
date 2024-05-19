3a55ea24b127c358e74552a44156d6eb5301e610
omega-jean <jeanalex1405@gmail.com>
2024-05-19 08:45:10 +0000

    a class Square that defines a square by: (based on 4-square.py)

---
+++ python-classes/5-square.py
@@ -0,0 +1,37 @@
+
+#!/usr/bin/python3
+"""Square generation module"""
+
+
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size=0):
+        """Initialize a new Square."""
+        self.__size = size
+
+    @property
+    def size(self):
+        """size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """value is the square length"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the square area"""
+        return(self.__size * self.__size)
+
+    def my_print(self):
+        """Prints text representation of square in hash chars."""
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print("#", end="")
+            print()
+        if self.__size is 0:
+            print()

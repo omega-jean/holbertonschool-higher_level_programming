e76fe9549c6e22523dddccf384132980ec28cf91
omega-jean <jeanalex1405@gmail.com>
2024-05-20 08:34:31 +0000

    just adding a description

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module contain a function that add two integers
+Args:
+    a (int): first parameter
+    b (int): second parameter
+"""
+
+
+def add_integer(a, b=98):
+    """add_integer function that add two integers
+    Returns:
+        int: Addition between a + b."""

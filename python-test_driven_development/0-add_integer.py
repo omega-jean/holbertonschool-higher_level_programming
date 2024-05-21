6a687cedcfbacd06c8b1ddefdf68b6fa63690951
omega-jean <jeanalex1405@gmail.com>
2024-05-21 09:25:05 +0000

    add code and test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,6 +7,8 @@ Args:
 
 
 def add_integer(a, b=98):
-    """add_integer function that add two integers
-    Returns:
-        int: Addition between a + b."""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)

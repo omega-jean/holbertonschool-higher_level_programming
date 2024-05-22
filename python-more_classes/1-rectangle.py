e5c355c4b46e2afc2a6210e81bd57fe11965bec4
omega-jean <jeanalex1405@gmail.com>
2024-05-22 12:39:19 +0000

    delete space

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,11 +1,10 @@
-
 #!/usr/bin/python3
 """Rectangle generation module"""
 
 
 class Rectangle:
     """class Rectangle that defines a rectangle"""
-    
+
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height

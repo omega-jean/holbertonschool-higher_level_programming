99119ea62a95b5e727e222c7096d3cb5c41c5013
omega-jean <jeanalex1405@gmail.com>
2024-05-23 13:48:06 +0000

    modify description

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -6,15 +6,23 @@ class Rectangle:
     """class Rectangle that defines a rectangle"""
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        """Method that initialize width and height fields
+
+        args:
+        height (int): rectangle height
+        width (int): rectangle width
+        """
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
+        """method of width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Setter method of width"""
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -23,10 +31,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """method of height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter method of height"""
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -34,9 +44,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Public method that returns the area of the rectangle"""
         return (self.__width * self.__height)
 
     def perimeter(self):
+        """Public method that returns the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
         return ((self.width * 2) + (self.height * 2))

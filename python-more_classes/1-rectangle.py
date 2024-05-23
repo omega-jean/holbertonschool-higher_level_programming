518ca5ad660829804f383007cd5aaec00be48ef0
omega-jean <jeanalex1405@gmail.com>
2024-05-23 13:26:51 +0000

    reverse width, height

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Method that initialize width and height fields"""
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):

469b45c2e556a8e95adfc23647c855fb18e0f6fa
omega-jean <jeanalex1405@gmail.com>
2024-05-23 13:43:15 +0000

    modify c in C

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -3,7 +3,7 @@
 
 
 class Rectangle:
-    """class Rectangle that defines a rectangle"""
+    """Class Rectangle that defines a rectangle"""
 
     def __init__(self, width=0, height=0):
         """Method that initialize width and height fields

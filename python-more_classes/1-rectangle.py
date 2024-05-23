ae3b1a77b7723c2cd9573a6b31eb7cc0f11de5ae
omega-jean <jeanalex1405@gmail.com>
2024-05-23 13:20:50 +0000

    modify description

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    """Setter method of width"""
     def width(self, value):
+        """Setter method of width"""
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -24,13 +24,13 @@ class Rectangle:
         self.__width = value
 
     @property
-    """method of height"""
     def height(self):
+        """method of height"""
         return self.__height
 
     @height.setter
-    """setter method of height"""
     def height(self, value):
+        """setter method of height"""
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:

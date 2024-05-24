f02ae9f34757d19a251757f3897e0410430c74d0
omega-jean <jeanalex1405@gmail.com>
2024-05-24 07:32:31 +0000

    add description

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -6,10 +6,17 @@ class Rectangle:
     """class Rectangle that defines a rectangle"""
 
     def __init__(self, width=0, height=0):
+        """Method that initialize width and height fields
+
+        args:
+        height (int): rectangle height
+        width (int): rectangle width
+        """
         self.__width = width
         self.__height = height
 
     def __str__(self):
+        """Public method that returns a printable rectangle"""
         if self.__width == 0 or self.__height == 0:
             return ""
         size = "#" * self.__width
@@ -19,14 +26,17 @@ class Rectangle:
         return "\n".join(rectangle)
 
     def __repr__(self):
+        """Public meyhod"""
         return "Rectangle({:d}, {:d})" .format(self.__width, self.__height)
 
     @property
     def width(self):
+        """method of width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Setter method of width"""
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -35,10 +45,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """method of height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter method of height"""
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -46,9 +58,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Public method that returns the area of the rectangle"""
         return (self.__width * self.__height)
 
     def perimeter(self):
+        """Public method that returns the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
         return ((self.width * 2) + (self.height * 2))

fa50929ade909978c57f922af2412328a0dc055a
omega-jean <jeanalex1405@gmail.com>
2024-05-24 12:35:49 +0000

    fix description

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -92,6 +92,6 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """classmethod square returns new Recatangle instance with
-        width == height == size."""
+        """classmethod square returns new Recatangle instance with\
+                width == height == size."""
         return cls(size, size)

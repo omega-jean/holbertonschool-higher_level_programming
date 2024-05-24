c44ecbcba54e970c89a4c9a538724cfe9053a142
omega-jean <jeanalex1405@gmail.com>
2024-05-24 09:49:17 +0000

    replace != by is not

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Setter method of width"""
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -37,7 +37,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Setter method of height"""
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")

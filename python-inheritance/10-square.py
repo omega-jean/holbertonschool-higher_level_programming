21e2b329781b227104abe552adbd27fe5d21cd80
omega-jean <jeanalex1405@gmail.com>
2024-05-25 09:06:26 +0000

    chnage code

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -41,6 +41,6 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """initialization"""
-        self.__size = size
         self.integer_validator("size", size)
-        super().__init__(size, size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)

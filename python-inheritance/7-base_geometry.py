9ef219407f8a62e5ef437835bea8058eafecd8e4
omega-jean <jeanalex1405@gmail.com>
2024-05-25 08:28:34 +0000

    a class BaseGeometry (based on 6-base_geometry.py).

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""BaseGeometry"""
+
+
+class BaseGeometry:
+    """create class BaseGeometry"""
+
+    def area(self):
+        """Public method that returns the area of BaseGeometry"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates that value is a positive integer."""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value  <= 0:
+            raise ValueError("{} must be greater than 0".format(name))

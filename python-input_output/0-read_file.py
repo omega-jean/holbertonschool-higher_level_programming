7861f28c93a0caae15b2b583d8704c5b7397baac
omega-jean <jeanalex1405@gmail.com>
2024-05-27 08:43:56 +0000

    change name

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,7 +2,8 @@
 """fonction read-file"""
 
 def read_file(filename=""):
-    """function that reads a text file in UTF-8"""
-    with open(filename, 'r', encoding="utf-8") as file:
-        content = file.read()
-        print(content, end='')
+    """Read a UTF-8 text file and print its content to stdout."""
+    with open(filename, 'r', encoding='utf-8') as f:
+        read_file = f.read()
+        print(read_file, end="")
+

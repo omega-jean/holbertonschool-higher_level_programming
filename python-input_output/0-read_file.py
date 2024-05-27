5d9a955608bce8f0b9bf4d258706e1cfd0272666
omega-jean <jeanalex1405@gmail.com>
2024-05-27 09:08:24 +0000

    fix pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """fonction read-file"""
 
+
 def read_file(filename=""):
     """Read a UTF-8 text file and print its content to stdout."""
     with open(filename, 'r', encoding='utf-8') as f:
         read_file = f.read()
         print(read_file, end="")
-

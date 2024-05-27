87b7ebcd90cb4ef7e06a52d09ed0f88ccc013f7f
omega-jean <jeanalex1405@gmail.com>
2024-05-27 13:45:30 +0000

    add If the file doesn’t exist, it should be created

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+"""script that adds all arguments to a Python list"""
 
 
 import json
@@ -13,6 +13,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 list_0 = []
 list_1 = sys.argv[1:]
-if os.path.exists("add_item.json"):
+if not os.path.exists("add_item.json"):
+    with open(file_name, 'w') as file:
+        json.dump([], file)
+else:
     list_0 = load_from_json_file("add_item.json")
 save_to_json_file(list_0 + list_1, "add_item.json")

95097c2f09e62da153e2638bf85aca0215b1277b
omega-jean <jeanalex1405@gmail.com>
2024-05-27 11:55:36 +0000

    add encoding="utf-8"

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -8,5 +8,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """Writes an object to a text file using\
             a JSON representation."""
-    with open(filename, "w") as file:
+    with open(filename, "w", encoding="utf-8") as file:
         json.dump(my_obj, file)

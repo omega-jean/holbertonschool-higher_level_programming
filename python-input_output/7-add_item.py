f7c98c51f1db61dbd2aeda2c06ff2f7169356d59
omega-jean <jeanalex1405@gmail.com>
2024-05-28 07:17:27 +0000

    modify code

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,7 +2,6 @@
 """script that adds all arguments to a Python list"""
 
 
-import json
 import sys
 import os.path
 
@@ -13,9 +12,6 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 list_0 = []
 list_1 = sys.argv[1:]
-if not os.path.exists("add_item.json"):
-    with open(file_name, 'w') as file:
-        json.dump([], file)
-else:
+if os.path.exists("add_item.json"):
     list_0 = load_from_json_file("add_item.json")
 save_to_json_file(list_0 + list_1, "add_item.json")

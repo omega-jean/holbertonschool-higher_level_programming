42760b9064e81aca4b66de782f2468d66c15182e
omega-jean <jeanalex1405@gmail.com>
2024-05-28 13:15:42 +0000

    class Student that defines a student by: (based on 10-student.py)

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""student generation modul"""
+
+
+class Student:
+    """create new class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns the dictionary description with simple data structure"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            result = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    result[key] = value
+            return result
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)

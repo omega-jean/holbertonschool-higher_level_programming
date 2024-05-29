a303406003707b72220164d56b19ee2218bef702
omega-jean <jeanalex1405@gmail.com>
2024-05-29 07:56:02 +0000

    a basic serialization module that adds the functionality to serialize a Python dictionary to a JSON file and deserialize the JSON file to recreate the Python Dictionary.

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""a basic serialization module that adds the functionality\
+    to serialize a Python dictionary to a JSON file and\
+    deserialize the JSON file to recreate the Python Dictionary."""
+
+
+def serialize_and_save_to_file(data, filename):
+    """Serialize a Python dictionary to a JSON file."""
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file)
+
+
+def load_and_deserialize(filename):
+    """Load and deserialize a JSON file to recreate a Python dictionary."""
+    with open(filename, 'r') as file:
+        read_file = json.load(file)
+        return read_file

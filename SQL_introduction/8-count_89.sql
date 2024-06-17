b2a7e4ad9f8c6e2e214495dd27e097daa1514515
omega-jean <jeanalex1405@gmail.com>
2024-06-17 15:39:57 +0200

    modify script

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- 0x0D. SQL - Introduction, task 8. Count 89
 -- script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0
-SELECT COUNT(id) FROM first_table;
+SELECT COUNT(*) FROM first_table WHERE id=89;

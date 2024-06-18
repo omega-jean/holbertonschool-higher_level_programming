5a7577556dbfdae33d836d46042158a2924227f8
omega-jean <jeanalex1405@gmail.com>
2024-06-18 11:17:13 +0200

    script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- 0x0D. SQL - Introduction, task 10. List by best
+-- script that lists all records of the table second_table of the database hbtn_0c_0
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;

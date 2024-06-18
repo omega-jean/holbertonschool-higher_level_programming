139e8e06b3f3eef57be434a6f446db52058440af
omega-jean <jeanalex1405@gmail.com>
2024-06-18 13:16:20 +0200

    script that updates the score of Bob to 10 in the table second_table.

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- 0x0D. SQL - Introduction, task 12. Cheating is bad
+-- script that updates the score of Bob to 10 in the table second_table.
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';

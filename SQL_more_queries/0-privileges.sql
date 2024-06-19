7ce8f8e01f982b7972eaf8e40ed5d2855ee27bca
omega-jean <jeanalex1405@gmail.com>
2024-06-19 11:29:02 +0200

    script that lists all privileges of the MySQL users user_0d_1 and user_0d_2

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- 0x0D. SQL - Introduction, task 1. My privileges!
+-- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

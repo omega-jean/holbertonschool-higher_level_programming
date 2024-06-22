555ca8d037513807d5aad12a6d1341195ad04432
omega-jean <jeanalex1405@gmail.com>
2024-06-22 08:45:38 +0200

    fix requirment

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""Task 2"""
+"""Task 2 - Filter states by user input"""
 
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
+def main():
+    """
+    Connects to a MySQL database and retrieves all values in the states table
+    where the name matches the argument passed. Results are displayed in ascending
+    order by states.id.
+    """
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
@@ -13,10 +18,13 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
                          passwd=mysql_password, db=database_name)
     cursor = db.cursor()
-    cursor.execute("SELECT * \
-                 FROM state WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
     states = cursor.fetchall()
     for state in states:
         print(state)
     cursor.close()
     db.close()
+
+if __name__ == "__main__":
+    main()

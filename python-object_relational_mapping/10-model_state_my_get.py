b0481f5c8dc498b28983e85b79d47ac818ae3b3d
omega-jean <jeanalex1405@gmail.com>
2024-06-23 09:07:22 +0200

    reload

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,11 +1,10 @@
-
 #!/usr/bin/python3
 """task 10"""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
 if __name__ == "__main__":
     username = sys.argv[1]
@@ -15,7 +14,6 @@ if __name__ == "__main__":
 
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         username, password, database), pool_pre_ping=True)
-
     Session = sessionmaker(bind=engine)
     session = Session()
 

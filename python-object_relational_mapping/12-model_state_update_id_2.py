8828a5eaab9041cdfb902ca46086f44ca7a25f3d
omega-jean <jeanalex1405@gmail.com>
2024-06-23 09:13:48 +0200

    task 12

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""task 12"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(f'mysql+mysqldb://{username}:\
+                           {password}@localhost:3306/{db_name}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_to_update = session.query(State).filter_by(id=2).first()
+
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+        session.commit()
+    session.close()
